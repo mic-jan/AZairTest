a3a59f38716fa929c7c3a9c3790b1c275a545c4c
Michal Janski <michaljanski@gmail.com>
2022-06-07 22:39:36 +0200

    WebDriverManager implemented

--- src/test/java/stepDefinitions/ResultPageStepDefinition.java
+++ src/test/java/stepDefinitions/ResultPageStepDefinition.java
@@ -1,9 +1,7 @@
 package stepDefinitions;
 
 import io.cucumber.java.en.And;
-import org.openqa.selenium.WebDriver;
 import pageObjects.ResultPage;
-import pageObjects.SearchPage;
 import stepDefinitions.core.DriverClass;
 
 public class ResultPageStepDefinition {
@@ -16,13 +14,13 @@ public class ResultPageStepDefinition {
 
     @And("Results are displayed")
     public void resultsAreDisplayed() throws InterruptedException {
-        Thread.sleep(2500);
+        Thread.sleep(1000);
     }
 
     @And("Book cheapest result")
     public void bookCheapestResult() throws InterruptedException {
         resultPage.bookFirstResult();
-        Thread.sleep(10000);
+        Thread.sleep(1000);
     }
 
     @And("Switch to second tab")
